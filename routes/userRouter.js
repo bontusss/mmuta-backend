a314cf0642a33ba2fe72d4d8bd3a9c2d749817da
bontusss <ikwecheghu@gmail.com>
2023-05-17 10:45:02 +0000

    removing duplicated files

--- routes/userRouter.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const { ping } = require('../controllers/testController');
-const { verifyToken } = require("../middlewares/verifyToken");
-const router = express.Router();
-router.get('/ping', verifyToken, ping);
-module.exports = router;
